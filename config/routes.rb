53906c2928ad011f89eacba22cc28f018c93fb9d
skylive <william.guezennec@gmail.com>
2016-05-05 00:20:51 +0000

    add members db

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root              'static_pages#home'
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
+  get 'members' =>  'members#show'
   
 end
